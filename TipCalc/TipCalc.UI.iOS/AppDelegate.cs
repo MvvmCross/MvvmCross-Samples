ff4cbcaac1750b643386b90c95860fa8d387e474
Jonathan Stoneman <jon@stoneman.me.uk>
2016-01-17 14:21:26 +0000

    New TipCalc - iOS project

---
+++ TipCalc/TipCalc.UI.iOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+using UIKit;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform;
+using MvvmCross.Core.ViewModels;
+
+namespace TipCalc.UI.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var presenter = new MvxIosViewPresenter(this, Window);
+
+            var setup = new Setup(this, presenter);
+            setup.Initialize();
+
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            startup.Start();
+
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
+
+
