2ce447c4e77d49a070f839da748abd3037095758
Jonathan Stoneman <jon@stoneman.me.uk>
2016-01-18 21:20:20 +0000

    New TipCalc - WPF project

---
+++ TipCalc/TipCalc.UI.Wpf/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Wpf.Views;
+
+namespace TipCalc.UI.Wpf
+{
+    public partial class App : Application
+    {
+        bool _setupComplete;
+
+        void DoSetup()
+        {
+            var presenter = new MvxSimpleWpfViewPresenter(MainWindow);
+
+            var setup = new Setup(Dispatcher, presenter);
+            setup.Initialize();
+
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
+
+            _setupComplete = true;
+        }
+
+        protected override void OnActivated(System.EventArgs e)
+        {
+            if (!_setupComplete)
+                DoSetup();
+
+            base.OnActivated(e);
+        }
+    }
+}
---
+++ TipCalc/TipCalc.UI.Wpf/Views/TipView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Wpf.Views;
+
+namespace TipCalc.UI.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for TipView.xaml
+    /// </summary>
+    public partial class TipView : MvxWpfView
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
