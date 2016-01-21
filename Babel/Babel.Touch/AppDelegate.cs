e6bda511fbbd68f030a8eda89bf436279328f6a5
Clinton Rocksmith <crocksmith@titaniumtech.com.au>
2016-01-21 18:06:25 +1100

    Fix iOS
    Fixed iOS build and updated from mono touch

--- Babel/Babel.Touch/AppDelegate.cs
+++ Babel/Babel.Touch/AppDelegate.cs
@@ -1,26 +1,29 @@
-using Cirrious.CrossCore;
-using Cirrious.MvvmCross.ViewModels;
+using Foundation;
+using UIKit;
+using MvvmCross.Platform;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
 
 namespace Babel.Touch
 {
-    [Register("AppDelegate")]
-    public partial class AppDelegate : MvxApplicationDelegate
-    {
-        private UIWindow _window;
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : MvxApplicationDelegate
+	{
+		private UIWindow _window;
 
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            _window = new UIWindow(UIScreen.MainScreen.Bounds);
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			_window = new UIWindow (UIScreen.MainScreen.Bounds);
 
-            var setup = new Setup(this, _window);
-            setup.Initialize();
+			var setup = new Setup (this, _window);
+			setup.Initialize ();
 
-            var startup = Mvx.Resolve<IMvxAppStart>();
-            startup.Start();
+			var startup = Mvx.Resolve<IMvxAppStart> ();
+			startup.Start ();
 
-            _window.MakeKeyAndVisible();
+			_window.MakeKeyAndVisible ();
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
--- Babel/Babel.Touch/Setup.cs
+++ Babel/Babel.Touch/Setup.cs
@@ -1,27 +1,30 @@
-using Cirrious.MvvmCross.Localization;
+using MvvmCross.iOS.Platform;
+using UIKit;
+using MvvmCross.Localization;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Babel.Touch
 {
-    public class Setup : MvxTouchSetup
-    {
-        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
-            : base(applicationDelegate, window)
-        {
-        }
+	public class Setup : MvxIosSetup
+	{
+		public Setup (MvxApplicationDelegate applicationDelegate, UIWindow window)
+			: base (applicationDelegate, window)
+		{
+		}
 
-        protected override System.Collections.Generic.List<System.Reflection.Assembly> ValueConverterAssemblies
-        {
-            get
-            {
-                var toReturn = base.ValueConverterAssemblies;
-                toReturn.Add(typeof(MvxLanguageConverter).Assembly);
-                return toReturn;
-            }
-        }
+		protected override IEnumerable<System.Reflection.Assembly> ValueConverterAssemblies {
+			get {
+				var toReturn = base.ValueConverterAssemblies.ToList ();
+				toReturn.Add (typeof(MvxLanguageConverter).Assembly);
+				return toReturn;
+			}
+		}
 
-        protected override Cirrious.MvvmCross.ViewModels.IMvxApplication CreateApp()
-        {
-            return new Core.App();
-        }
-    }
+		protected override MvvmCross.Core.ViewModels.IMvxApplication CreateApp ()
+		{
+			return new Core.App ();
+		}
+	}
 }
