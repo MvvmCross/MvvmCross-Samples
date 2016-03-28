ee91e0f22a27a8b188b4d97ad580a96a7c1f658e
benjamin hysell <ben.hysell@gmail.com>
2016-03-28 12:46:33 -0400

    orgainized views and viewmodels, added info view iOS, added third view

---
+++ XPlatformMenus/XPlatformMenus.iOS/Views/Home/InfoView.cs
@@ -0,0 +1,65 @@
+﻿using Cirrious.FluentLayouts.Touch;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using UIKit;
+using XPlatformMenus.Core.ViewModels;
+using XPlatformMenus.Touch.Panels;
+
+namespace XPlatformMenus.Touch.Views
+{
+    [Register("InfoView")]
+    [PanelPresentation(PanelEnum.Center, PanelHintType.ActivePanel, true)]
+    public class InfoView : BaseViewController<InfoViewModel>
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var viewModel = this.ViewModel;
+
+            var scrollView = new UIScrollView(View.Frame)
+            {
+                ShowsHorizontalScrollIndicator = false,
+                AutoresizingMask = UIViewAutoresizing.FlexibleHeight
+            };
+            var label = new UILabel();
+            label.Text = "Info View";
+            var thirdButton = new UIButton();
+            thirdButton.SetTitle("Show Third ViewModel", UIControlState.Normal);
+            thirdButton.BackgroundColor = UIColor.Blue;
+            scrollView.AddSubviews(label, thirdButton);
+
+            Add(scrollView);
+
+            View.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            View.AddConstraints(
+                scrollView.AtLeftOf(View),
+                scrollView.AtTopOf(View),
+                scrollView.WithSameWidth(View),
+                scrollView.WithSameHeight(View));
+            scrollView.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            var set = this.CreateBindingSet<InfoView, InfoViewModel>();
+            set.Bind(thirdButton).To(vm => vm.ShowThirdViewModelCommand);
+            set.Apply();
+
+            scrollView.AddConstraints(
+
+                label.Below(scrollView).Plus(10),
+                label.WithSameWidth(scrollView),
+                label.WithSameLeft(scrollView),
+
+                thirdButton.Below(label).Plus(10),
+                thirdButton.WithSameWidth(label),
+                thirdButton.WithSameLeft(label)
+                );
+
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            Title = "Info View";
+            base.ViewWillAppear(animated);
+        }
+    }
+}
