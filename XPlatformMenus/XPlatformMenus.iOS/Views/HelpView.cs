5d505ee851ab17ee389bc08d13ab62a737b9c74f
benjamin hysell <ben.hysell@gmail.com>
2016-03-28 11:57:17 -0400

    updated to 4.1.0 mvvmcross, added square corners for views iOS, fixed issue where pressing 'home' would cause a blank screen

--- XPlatformMenus/XPlatformMenus.iOS/Views/HelpView.cs
+++ XPlatformMenus/XPlatformMenus.iOS/Views/HelpView.cs
@@ -5,7 +5,7 @@ using XPlatformMenus.Touch.Panels;
 namespace XPlatformMenus.Touch.Views
 {
     [Register("HelpView")]
-    [PanelPresentation(PanelEnum.Center, PanelHintType.ActivePanel, true)]
+    [PanelPresentation(PanelEnum.Center, PanelHintType.ResetRoot, true)]
     public class HelpView : BaseViewController<HelpAndFeedbackViewModel>
     {
         public override void ViewWillAppear(bool animated)
