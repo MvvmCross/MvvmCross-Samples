403a2f9a7d5a718d55ca564ffd330a57a22ca09c
Seifer <seed-seifer@ya.ru>
2016-04-11 15:38:19 +0300

    Demonstration of custom plugin implementation loading in Android project

---
+++ ValueConversion/ValueConversion.Droid/CustomColorPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Platform;
+using MvvmCross.Platform.UI;
+using MvvmCross.Platform.Plugins;
+using MvvmCross.Plugins.Color.Droid;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Plugins.Color.Droid.BindingTargets;
+
+namespace SomeAbsolutelty.Unconventional.Namespace
+{
+    public class CustomColorPlugin: IMvxPlugin
+    {
+        public void Load()
+        {
+            Console.WriteLine ("-------- This plugin is loaded instead of the default");
+            Mvx.RegisterSingleton<IMvxNativeColor>(new MvxAndroidColor());
+            Mvx.CallbackWhenRegistered<IMvxTargetBindingFactoryRegistry>(RegisterDefaultBindings);
+        }
+
+        private void RegisterDefaultBindings()
+        {
+            var helper = new MvxDefaultColorBindingSet();
+            helper.RegisterBindings();
+        }
+    }
+}
+
