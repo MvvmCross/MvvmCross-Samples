ef9691e1ec1bbfd46a12d45dec1ba5973e698e03
Tim Uy <tim@loqu8.com>
2016-06-20 15:23:42 -0700

    add various views (not hookedup) to XPatformMenus WPF

---
+++ XPlatformMenus/XPlatformMenus.WPF/Views/Base/BaseView.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPlatformMenus.WPF.Views
+{
+    public class BaseView : MvxWpfView
+    {
+    }
+}
---
+++ XPlatformMenus/XPlatformMenus.WPF/Views/Login/LoginView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MvvmCross.Wpf.Views;
+using XPlatformMenus.Core.ViewModels;
+
+namespace XPlatformMenus.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : BaseView
+    {
+        public new LoginViewModel ViewModel
+        {
+            get { return (LoginViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
