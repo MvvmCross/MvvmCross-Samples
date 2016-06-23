6b3c57382dbb08f06ecb9a1797e614c94ad55f28
Tim Uy <tim@loqu8.com>
2016-06-22 20:16:54 -0700

    setup TipCalc.UI.Mac

---
+++ TipCalc/TipCalc.UI.Mac/Views/TipView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+using AppKit;
+using MvvmCross.Binding.Mac.Views;
+
+namespace TipCalc.UI.Mac
+{
+	public partial class TipView : MvxView
+	{
+		#region Constructors
+
+		// Called when created from unmanaged code
+		public TipView(IntPtr handle) : base(handle)
+		{
+			Initialize();
+		}
+
+		// Called when created directly from a XIB file
+		[Export("initWithCoder:")]
+		public TipView(NSCoder coder) : base(coder)
+		{
+			Initialize();
+		}
+
+		// Shared initialization code
+		void Initialize()
+		{
+		}
+
+		#endregion
+	}
+}
