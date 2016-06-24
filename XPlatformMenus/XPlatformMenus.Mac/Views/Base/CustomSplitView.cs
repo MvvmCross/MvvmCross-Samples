99e17688c7f5b89cd0af04c4261f3839f5f43c63
Tim Uy <tim@loqu8.com>
2016-06-23 17:41:20 -0700

    fix splitview resizing

---
+++ XPlatformMenus/XPlatformMenus.Mac/Views/Base/CustomSplitView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace XPlatformMenus.Mac.Views
+{
+	[Register("CustomSplitView")]
+	public class CustomSplitView : NSSplitView
+	{
+		#region Constructors
+
+		// Called when created from unmanaged code
+		public CustomSplitView(IntPtr handle) : base(handle)
+		{
+			Initialize();
+		}
+
+		// Called when created directly from a XIB file
+		public CustomSplitView(NSCoder coder) : base(coder)
+		{
+			Initialize();
+		}
+
+		// Shared initialization code
+		void Initialize()
+		{
+		}
+
+		#endregion
+	}
+}
+
