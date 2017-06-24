98fae2c8a731a252ec96f7c7b37d1273ddffb129
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-06-24 18:01:24 -0300

    Edited iOS TwitterCoverImageView: Remove hardcoded width and unused code

--- StarWarsSample/StarWarsSample.iOS/CustomControls/TwitterCoverImageView.cs
+++ StarWarsSample/StarWarsSample.iOS/CustomControls/TwitterCoverImageView.cs
@@ -15,7 +15,6 @@ namespace StarWarsSample.iOS.CustomControls
 
         private UIScrollView scrollView;
         private UIView topView;
-        //private List<UIImage> blurredImages;
 
         public TwitterCoverImageView()
         {
@@ -43,7 +42,6 @@ namespace StarWarsSample.iOS.CustomControls
         {
             topView = top;
             CoverViewHeight = DefaultCoverViewHeight;
-            //blurredImages = new List<UIImage>(20);
             ContentMode = UIViewContentMode.ScaleAspectFill;
             ClipsToBounds = true;
         }
@@ -70,13 +68,14 @@ namespace StarWarsSample.iOS.CustomControls
             set
             {
                 base.Image = value;
-                //PrepareForBlurImages();
             }
         }
 
         public override void LayoutSubviews()
         {
             base.LayoutSubviews();
+
+            var windowWidth = UIApplication.SharedApplication.Delegate.GetWindow().Bounds.Width;
 
             if (ScrollView.ContentOffset.Y < 0)
             {
@@ -85,41 +84,20 @@ namespace StarWarsSample.iOS.CustomControls
                 nfloat topViewHeight = 0;
                 if (topView != null)
                 {
-                    topView.Frame = new CGRect(0, -offset, 320, topView.Bounds.Size.Height);
+                    topView.Frame = new CGRect(0, -offset, windowWidth, topView.Bounds.Size.Height);
                     topViewHeight = topView.Bounds.Size.Height;
                 }
-                Frame = new CGRect(-offset, -offset + topViewHeight, 320 + offset * 2, CoverViewHeight + offset);
-
-                //int index = (int)offset / 10;
-                //if (index < 0)
-                //{
-                //    index = 0;
-                //}
-                //else if (index >= blurredImages.Count)
-                //{
-                //    index = blurredImages.Count - 1;
-                //}
-                //var image = blurredImages[index];
-                //if (Image != image)
-                //{
-                //    base.Image = image;
-                //}
+                Frame = new CGRect(-offset, -offset + topViewHeight, windowWidth + offset * 2, CoverViewHeight + offset);
             }
             else
             {
                 nfloat topViewHeight = 0;
                 if (topView != null)
                 {
-                    topView.Frame = new CGRect(0, 0, 320, topView.Bounds.Size.Height);
+                    topView.Frame = new CGRect(0, 0, windowWidth, topView.Bounds.Size.Height);
                     topViewHeight = topView.Bounds.Size.Height;
                 }
-                Frame = new CGRect(0, topViewHeight, 320, CoverViewHeight);
-
-                //var image = blurredImages[0];
-                //if (Image != image)
-                //{
-                //    base.Image = image;
-                //}
+                Frame = new CGRect(0, topViewHeight, windowWidth, CoverViewHeight);
             }
         }
 
@@ -138,105 +116,5 @@ namespace StarWarsSample.iOS.CustomControls
         {
             SetNeedsLayout();
         }
-
-        /*
-        private void PrepareForBlurImages()
-        {
-            blurredImages.Clear();
-            float num = 0.1f;
-            blurredImages.Add(Image);
-            for (int i = 0; i < 20; i++)
-            {
-                blurredImages.Add(Blur(Image, num));
-                num += 0.04f;
-            }
-        }
-        */
-
-        /*
-        private static UIImage Blur(UIImage image, float blurRadius)
-        {
-            if (image.Size.Width < 1 || image.Size.Height < 1)
-            {
-                Debug.WriteLine(@"*** error: invalid size:({0} x {1}). Both dimensions must be >= 1: {2}", image.Size.Width, image.Size.Height, image);
-                return null;
-            }
-            if (image.CGImage == null)
-            {
-                Debug.WriteLine(@"*** error: image must be backed by a CGImage: {0}", image);
-                return null;
-            }
-
-            if (blurRadius < 0f || blurRadius > 1f)
-            {
-                blurRadius = 0.5f;
-            }
-            var inputRadius = blurRadius * UIScreen.MainScreen.Scale;
-            var boxSize = (uint)(inputRadius * 40);
-            boxSize = boxSize - (boxSize % 2) + 1;
-
-            var imageRect = new CGRect(CGPoint.Empty, image.Size);
-
-            UIImage effectImage;
-            UIGraphics.BeginImageContextWithOptions(image.Size, false, UIScreen.MainScreen.Scale);
-            {
-                var contextIn = UIGraphics.GetCurrentContext();
-                //              contextIn.ScaleCTM(1.0f, -1.0f);
-                //              contextIn.TranslateCTM(0, -image.Size.Height);
-                contextIn.DrawImage(imageRect, image.CGImage);
-                var effectInContext = contextIn.AsBitmapContext();
-
-                var effectInBuffer = new vImageBuffer
-                {
-                    Data = effectInContext.Data,
-                    Width = (int)effectInContext.Width,
-                    Height = (int)effectInContext.Height,
-                    BytesPerRow = (int)effectInContext.BytesPerRow
-                };
-
-                UIGraphics.BeginImageContextWithOptions(image.Size, false, UIScreen.MainScreen.Scale);
-                {
-                    var effectOutContext = UIGraphics.GetCurrentContext().AsBitmapContext();
-                    var effectOutBuffer = new vImageBuffer
-                    {
-                        Data = effectOutContext.Data,
-                        Width = (int)effectOutContext.Width,
-                        Height = (int)effectOutContext.Height,
-                        BytesPerRow = (int)effectOutContext.BytesPerRow
-                    };
-
-                    vImage.BoxConvolveARGB8888(ref effectInBuffer, ref effectOutBuffer, IntPtr.Zero, 0, 0, boxSize, boxSize, Pixel8888.Zero, vImageFlags.EdgeExtend);
-                    vImage.BoxConvolveARGB8888(ref effectOutBuffer, ref effectInBuffer, IntPtr.Zero, 0, 0, boxSize, boxSize, Pixel8888.Zero, vImageFlags.EdgeExtend);
-                    vImage.BoxConvolveARGB8888(ref effectInBuffer, ref effectOutBuffer, IntPtr.Zero, 0, 0, boxSize, boxSize, Pixel8888.Zero, vImageFlags.EdgeExtend);
-
-                    effectImage = UIGraphics.GetImageFromCurrentImageContext();
-
-                    UIGraphics.EndImageContext();
-                }
-
-                UIGraphics.EndImageContext();
-            }
-
-            // Setup up output context
-            UIImage outputImage;
-            UIGraphics.BeginImageContextWithOptions(image.Size, false, UIScreen.MainScreen.Scale);
-            {
-                var outputContext = UIGraphics.GetCurrentContext();
-                //              outputContext.ScaleCTM(1, -1);
-                //              outputContext.TranslateCTM(0, -image.Size.Height);
-
-                // Draw base image
-                outputContext.SaveState();
-                outputContext.DrawImage(imageRect, effectImage.CGImage);
-                outputContext.RestoreState();
-
-                outputImage = UIGraphics.GetImageFromCurrentImageContext();
-
-                UIGraphics.EndImageContext();
-            }
-
-            return outputImage;
-        }
-        */
     }
 }
