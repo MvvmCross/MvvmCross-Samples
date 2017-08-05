8a73600e9865393c03f248def43ff0b15d8c341e
Madugula Venkataramana <madugulavenkataramana@Venkataramanas-Mac-mini.local>
2017-08-05 13:58:40 +0530

    Added the Missing MethodBindinPluginBootstrap file

---
+++ OldSamples/QuickLayout/QuickLayout.Touch/Bootstrap/MethodBindingPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace QuickLayout.Touch.Bootstrap
+{
+    public class MethodBindingPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.MethodBinding.PluginLoader>
+    {
+    }
+}
