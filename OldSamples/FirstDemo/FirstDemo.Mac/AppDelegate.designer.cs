45cc1cb64df36c54dbfa2c4f7c805b91aea796a6
Madugula Venkataramana <madugulavenkataramana@Venkataramanas-Mac-mini.local>
2017-08-06 23:52:39 +0530

    Migrated FirstDemo Project to Xamarin.Unified Project Deleted References from Local folders Updated Nuget Pacakages TODO : Fix the "*** Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: '-[NSNib _initWithNibNamed:bundle:options:] could not load the nibName: FirstViewController in bundle (null).' exception.

--- OldSamples/FirstDemo/FirstDemo.Mac/AppDelegate.designer.cs
+++ OldSamples/FirstDemo/FirstDemo.Mac/AppDelegate.designer.cs
@@ -2,7 +2,7 @@
 namespace FirstDemo.Mac
 {
 	// Should subclass MonoMac.AppKit.NSResponder
-	[MonoMac.Foundation.Register("AppDelegate")]
+	[Foundation.Register("AppDelegate")]
 	public partial class AppDelegate
 	{
 	}
