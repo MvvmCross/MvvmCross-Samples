77fdbeba88be4d969ee6c018fbf54a71e4bc2683
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-03-16 09:48:05 -0300

    Edited iOS PlanetsView: Use multiple cells

---
+++ StarWarsSample/StarWarsSample.iOS/Views/Cells/WhiteNameTableViewCell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Cirrious.FluentLayouts.Touch;
+using MvvmCross.Binding.BindingContext;
+using UIKit;
+
+namespace StarWarsSample.iOS.Views.Cells
+{
+    public class WhiteNameTableViewCell : BaseTableViewCell
+    {
+        private const float PADDING = 12f;
+
+        private UILabel _lblName;
+
+        public WhiteNameTableViewCell(IntPtr handle) : base(handle)
+        {
+        }
+
+        protected override void CreateView()
+        {
+            base.CreateView();
+
+            SelectionStyle = UITableViewCellSelectionStyle.None;
+
+            _lblName = new UILabel
+            {
+                TextColor = UIColor.White,
+                Font = UIFont.SystemFontOfSize(15f, UIFontWeight.Bold)
+            };
+
+            BackgroundColor = UIColor.Clear;
+            ContentView.AddSubview(_lblName);
+            ContentView.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            this.DelayBind(
+                () =>
+                {
+                    this.AddBindings(_lblName, "Text Name");
+                });
+        }
+
+        protected override void CreateConstraints()
+        {
+            base.CreateConstraints();
+
+            ContentView.AddConstraints(
+                _lblName.AtLeftOf(ContentView, PADDING),
+                _lblName.AtTopOf(ContentView, PADDING),
+                _lblName.AtBottomOf(ContentView, PADDING),
+                _lblName.AtRightOf(ContentView, PADDING)
+            );
+        }
+    }
+}
