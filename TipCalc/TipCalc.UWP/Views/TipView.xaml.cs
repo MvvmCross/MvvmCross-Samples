15f49b856b5c257cf6159edeb1657bbf5c740072
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-04-29 18:52:30 -0300

    TipCalc: Remove WindowsCommon and fix UWP sample

---
+++ TipCalc/TipCalc.UWP/Views/TipView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Uap.Views;
+using MvvmCross.ViewModels;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.UWP.Views
+{
+    [MvxViewFor(typeof(TipViewModel))]
+    public sealed partial class TipView : MvxWindowsPage
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
