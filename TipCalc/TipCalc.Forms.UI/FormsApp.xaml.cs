1583717c2904ce1cb0dc0b7f6bc52ac6adc5ac70
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-04-29 16:44:08 -0300

    Added TipCalc.Forms.UI project

---
+++ TipCalc/TipCalc.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TipCalc.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
