c2bffd161362d7e0f1d154c83aa0a91654ec7a5c
FabriBertani <fabricio.bertani@dgenix.com>
2019-04-21 01:34:09 -0300

    Added StarWarsSampleUWP project to forms.

---
+++ StarWarsSample/StarWarsSample.Forms/StarWarsSample.Forms.UWP/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCross.Forms.Platforms.Uap.Core;
+using MvvmCross.Forms.Platforms.Uap.Views;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+
+namespace StarWarsSample.Forms.UWP
+{
+    public sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs activationArgs)
+        {
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
+            OxyPlot.Xamarin.Forms.Platform.UWP.PlotViewRenderer.Init();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = false;
+
+            base.OnLaunched(activationArgs);
+        }
+    }
+
+    public abstract class StarWarsSampleApp : MvxWindowsApplication<MvxFormsWindowsSetup<Core.App, Forms.App>, Core.App, Forms.App, MainPage>
+    {
+    }
+}
---
+++ StarWarsSample/StarWarsSample.Forms/StarWarsSample.Forms.UWP/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.ViewManagement;
+
+namespace StarWarsSample.Forms.UWP
+{    
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ForegroundColor = Windows.UI.Colors.White;
+            titleBar.BackgroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonForegroundColor = Windows.UI.Colors.White;
+            titleBar.ButtonBackgroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Colors.Red;
+            titleBar.ButtonHoverForegroundColor = Windows.UI.Colors.White;
+        }
+    }
+}
