8d6504364558ce6a5757ba43ecec9c96b7028d3c
Tom PoLáKoSz <polakosz@freemail.hu>
2019-05-14 12:46:10 +0200

    Upgrade Babel.Droid to MvvmCross 6.2.3

--- Babel/Babel.Droid/Views/SecondView.cs
+++ Babel/Babel.Droid/Views/SecondView.cs
@@ -1,16 +1,16 @@
-using Android.App;
+﻿using Android.App;
 using Android.OS;
 using MvvmCross.Platforms.Android.Views;
 
 namespace Babel.Droid.Views
 {
-    [Activity(Label = "View for FirstViewModel")]
-    public class FirstView : MvxActivity
+    [Activity(Label = "View for SecondViewModel")]
+    public class SecondView : MvxActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.FirstView);
+            SetContentView(Resource.Layout.SecondView);
         }
     }
 }
