3d0e829bd16f068d5787bfa8033bae3cbe2fd272
Sergey Nechaev <6499856+snechaev@users.noreply.github.com>
2020-01-24 14:32:09 +0700

    #122 Update TipCalc.UI.Mac sample
    * added Mac project back to the solution
    * updated MvvmCross packages up to 6.2.2 to match Core project
    * updated namespaces in accordance with new MvvmCross version
    * updated setup/startup code in accordance with MvvmCross 6 requirements

--- TipCalc/TipCalc.UI.Mac/AppDelegate.cs
+++ TipCalc/TipCalc.UI.Mac/AppDelegate.cs
@@ -1,38 +1,18 @@
 ﻿using AppKit;
 using Foundation;
-using MvvmCross.Mac.Platform;
-using MvvmCross.Mac.Views.Presenters;
-using MvvmCross.Platform;
-using MvvmCross.Core.ViewModels;
-using CoreGraphics;
+using MvvmCross.Platforms.Mac.Core;
 
 namespace TipCalc.UI.Mac
 {
 	[Register("AppDelegate")]
-	public class AppDelegate : MvxApplicationDelegate
+	public class AppDelegate : MvxApplicationDelegate<MvxMacSetup<Core.App>, Core.App>
 	{
 		public NSWindow Window { get; set; }
 
 		public override void DidFinishLaunching(NSNotification notification)
 		{
-			Window = new NSWindow(new CGRect(0, 0, 400, 300),
-					  NSWindowStyle.Titled | NSWindowStyle.Resizable | NSWindowStyle.Closable,
-					  NSBackingStore.Buffered, false, NSScreen.MainScreen);        // how big?
-			Window.Title = "TipCalc";
-			Window.WillClose += (sender, e) =>
-			{
-				NSApplication.SharedApplication.Terminate(this);
-			};
-
-			var presenter = new MvxMacViewPresenter(this, Window);
-
-			var setup = new Setup(this, presenter);
-			setup.Initialize();
-
-			var startup = Mvx.Resolve<IMvxAppStart>();
-			startup.Start();
-
-			Window.MakeKeyAndOrderFront(this);
+			MvxMacSetupSingleton.EnsureSingletonAvailable(this, MainWindow).EnsureInitialized();
+			RunAppStart();
 		}
 
 		public override void WillTerminate(NSNotification notification)
