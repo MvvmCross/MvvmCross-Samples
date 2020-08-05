2741a980d9425485b9f662a4796198522a35f84b
Jan Zahradník <jan.zahradnik@gmail.com>
2020-08-05 22:52:41 +0200

    Broken WPF Forms version

---
+++ TipCalc/TipCalc.Forms.WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Forms.Platforms.Wpf.Core;
+using TipCalc.Forms.UI;
+
+namespace TipCalc.Forms.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxFormsWpfSetup<App, FormsApp>>();
+        }
+    }
+}
